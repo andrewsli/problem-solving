dbef13ce3c10020ab633c182519360ae3f4651fc
Andrew Li <andrewli0725@gmail.com>
2019-10-10 16:34:59 -0700

    leetcode #1 two sum in python

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,4 +1,4 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
+def twoSum(self, nums, target):
     seen = {}
     for idx, num in enumerate(nums, 0):
         if seen.get(target - num) == 0 or seen.get(target - num):
